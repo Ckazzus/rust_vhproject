7271224f1ca30dbf48adbc517fbf426588f1a84f
ckazzus <zuravkov5646@gmail.com>
2026-04-19 21:39:56 +0300

    listens to port 80 and gives an HTTP response

--- main.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    println!("hello, world!");
-    println!("hello, valuehost");
-
-    say_goodbye();
-}
-fn say_goodbye() {
-    println!("goodbye, world!");
-}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("0.0.0.0:80").unwrap();
+    println!("server start port 80");
+    
+    for stream in listener.incoming() {
+        handle_connection(stream.unwrap());
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    
+    println!("hello world");
+    
+    let response = "HTTP/1.1 200 OK\r\nContent-Length: 11\r\nConnection: close\r\n\r\nHello world";
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
